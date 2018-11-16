e9d808f916dd7196e9407314a8c028d80efdca16
David Mejia <franklin.mejia@epn.edu.ec>
2018-11-16 16:05:17 -0500

    Update Ciclo De Vida

--- ClicloDeVida/app/src/main/java/com/example/asemb/cliclodevida/ClicloVidaActivity.kt
+++ ClicloDeVida/app/src/main/java/com/example/asemb/cliclodevida/ClicloVidaActivity.kt
@@ -13,16 +13,22 @@ class ClicloVidaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cliclo_vida)
 
+        val tieneContadorGuardado=savedInstanceState?.get(SAVE_INSTANCE_CONTADOR)as Int?
+
+        if(tieneContadorGuardado!=null){
+                contador=tieneContadorGuardado
+                this.aumentarContador(0)
+        }
 
         c_Contador.setOnClickListener{
 
-            this.aumentarContador()
+            this.aumentarContador(1)
 
         }
     }
 
-    fun aumentarContador(){
-        contador=contador+1
+    fun aumentarContador(suma:Int){
+        contador=contador+suma
         text_Contador.text=contador.toString()
     }
 
@@ -58,9 +64,21 @@ class ClicloVidaActivity : AppCompatActivity() {
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState)
+
         Log.i("ciclo-vida","On onSaveInstanceState")
+
+        outState?.run {
+            putInt(SAVE_INSTANCE_CONTADOR,contador)
+        }
+        super.onSaveInstanceState(outState)
+
+
     }
 
+
+
+    companion object {
+        val SAVE_INSTANCE_CONTADOR="contador"
+    }
 
 }
