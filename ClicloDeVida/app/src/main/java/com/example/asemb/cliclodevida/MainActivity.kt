c1e16c921df38c8f974ffe39e52f582cc038a3f5
David Mejia <franklin.mejia@epn.edu.ec>
2018-11-23 15:16:42 -0500

    ListView

--- ClicloDeVida/app/src/main/java/com/example/asemb/cliclodevida/MainActivity.kt
+++ ClicloDeVida/app/src/main/java/com/example/asemb/cliclodevida/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.asemb.cliclodevida
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_cliclo_vida.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -12,6 +14,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val arregloUsuarios=ArrayList<User>();
+
+        val usuarioUno=User("David",29, Date(1986,6,10),12.00)
+        val usuarioDos=User("Evelyn",29, Date(1986,6,10),12.00)
+
+        arregloUsuarios.add(usuarioUno)
+        arregloUsuarios.add(usuarioDos)
+
+        var listView = findViewById<ListView>(R.id.recipe_list_view)
+
+
+
+        val listItems = arrayOfNulls<String>(arregloUsuarios.size)
+
+        for (i in 0 until arregloUsuarios.size) {
+            val recipe = arregloUsuarios[i]
+            listItems[i] = recipe.nomnbre
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems)
+        listView.adapter = adapter
+        
+        val adaptadorUsuarios= ArrayAdapter<User>(this,
+            android.R.layout.simple_spinner_item,
+            arregloUsuarios)
+
+        spinner_usuarios.adapter=adaptadorUsuarios;
 
 
         b_CicloDeVida.setOnClickListener{
